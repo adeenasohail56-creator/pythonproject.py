d8cbef2ff454462b586d08fbacea54504d3c4c63
ADEENA SOHAIL <adeenasohail56@gmail.com>
2026-08-23 10:42:10 -0700

    Update Guess the Number.py

--- Guess the Number.py
+++ Guess the Number.py
@@ -1,1 +1,19 @@
+import random
 
+secret_number = random.randint(1, 100)
+attempts = 0
+
+print("Welcome to the Number Guessing Game!")
+print("I have chosen a number between 1 and 100.")
+
+while True:
+    guess = int(input("Enter your guess: "))
+    attempts += 1
+
+    if guess < secret_number:
+        print("Too Low!")
+    elif guess > secret_number:
+        print("Too High!")
+    else:
+        print(f"Congratulations! You guessed the number in {attempts} attempts.")
+        break
