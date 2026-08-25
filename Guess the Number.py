d9675ed667bdc7c52b06a0624c252ed14b6e14eb
ADEENA SOHAIL <adeenasohail56@gmail.com>
2026-08-25 14:02:37 -0700

    Fix formatting issues in Guess the Number.py

--- Guess the Number.py
+++ Guess the Number.py
@@ -3,11 +3,9 @@ secret_number = random.randint(1, 100)
 attempts = 0
 print("Welcome to the Number Guessing Game!")
 print("I have chosen a number between 1 and 100.")
-
 while True:
     guess = int(input("Enter your guess: "))
     attempts += 1
-
     if guess < secret_number:
         print("Too Low!")
     elif guess > secret_number:
