a7c34a6380c5763374c9a90c8391094f32fe77d5
ADEENA SOHAIL <adeenasohail56@gmail.com>
2026-08-25 14:06:22 -0700

    Update passwordchecker.py

--- passwordchecker.py
+++ passwordchecker.py
@@ -1,1 +1,24 @@
+password = input("Enter your password: ")
 
+strength = 0
+
+if len(password) >= 8:
+    strength += 1
+
+if any(char.isdigit() for char in password):
+    strength += 1
+
+if any(char.isupper() for char in password):
+    strength += 1
+
+if any(char in "!@#$%^&*()" for char in password):
+    strength += 1
+
+print("\nPassword Analysis")
+
+if strength == 4:
+    print("Strong Password 💪")
+elif strength >= 2:
+    print("Medium Password 👍")
+else:
+    print("Weak Password ⚠️")
