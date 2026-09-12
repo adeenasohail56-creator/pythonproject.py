106abff06fd9afd1fabafd55c88ce52774c3f63e
ADEENA SOHAIL <adeenasohail56@gmail.com>
2026-09-12 13:21:00 -0700

    Add input prompt for ATM PIN entry

--- atmproject.py
+++ atmproject.py
@@ -3,6 +3,7 @@ balance = 10000
 attempts = 0
 while attempts < 5:
     entered_pin = input("Enter ATM PIN: ")
+    
     if entered_pin == pin:
         print("\nLogin Successful!")
         while True:
